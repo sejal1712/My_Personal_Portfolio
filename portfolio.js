b7e63c4ab155bb371cac305eb677d19534bf9a7a
Sejal Kushwaha <sejalkushwaha768@gmail.com>
2024-05-21 17:06:14 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,48 @@
+var typed = new Typed('#element', {
+  strings: ['Web Developer', 'Frontend Software Developer'],
+  typeSpeed: 70,
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('download-resume').addEventListener('click', function(){
+  const resumePath = 'Sejal_Kushwaha_Resume.pdf';
+
+  const link = document.createElement('a');
+  link.href = resumePath;
+
+  link.download = 'Sejal_Kushwaha_Resume.pdf';
+
+  document.body.appendChild(link);
+
+  link.click();
+
+  document.body.removeChild(link);
+});
+
+document.getElementById('visit-github').addEventListener('click', function(){
+  window.open('https://github.com/sejal1712', '_blank');
+});
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const faders = document.querySelectorAll('.fade-in');
+
+  const appearOptions = {
+    threshold: 0.5,
+    rootMargin: '0px 0px -50px 0px'
+  };
+
+  const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) return;
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target);
+    });
+  }, appearOptions);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+});
+
